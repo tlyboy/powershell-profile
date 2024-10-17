0a73582803b9fa9c2a4d2f6c4b92cabea438acee
Guany <1084072690@qq.com>
2024-10-17 21:20:21 +0800

    feat: initial

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,90 @@
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+Import-Module posh-git
+Import-Module git-aliases -DisableNameChecking
+Import-Module z
+
+Invoke-Expression (&starship init powershell)
+
+Set-Alias -Name la -Value Get-ChildItem
+
+function i {
+    param (
+        [string]$DirectoryName
+    )
+
+    Set-Location -Path "~\i\$DirectoryName"
+}
+
+#region conda initialize
+# !! Contents within this block are managed by 'conda init' !!
+If (Test-Path "C:\Users\tly\miniconda3\Scripts\conda.exe") {
+    (& "C:\Users\tly\miniconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ?{$_} | Invoke-Expression
+}
+#endregion
+
+fnm completions --shell powershell | Out-String | Invoke-Expression
+
+fnm env --use-on-cd --version-file-strategy=recursive --corepack-enabled --resolve-engines --shell powershell | Out-String | Invoke-Expression
+
+Remove-Item Alias:ni -Force -ErrorAction Ignore
+
+function nio {
+    ni --prefer-offline
+}
+
+function s {
+    nr start
+}
+
+function d {
+    nr dev
+}
+
+function b {
+    nr build
+}
+
+function bw {
+    nr build --watch
+}
+
+function t {
+    nr test
+}
+
+function tu {
+    nr test -u
+}
+
+function tw {
+    nr test --watch
+}
+
+function w {
+    nr watch
+}
+
+function p {
+    nr play
+}
+
+function c {
+    nr typecheck
+}
+
+function lint {
+    nr lint
+}
+
+function lintf {
+    nr lint --fix
+}
+
+function release {
+    nr release
+}
+
+function re {
+    nr release
+}
