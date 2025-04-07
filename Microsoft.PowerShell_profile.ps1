b1d44ca9c8f33f4d6ed6dee2d2bff79084aa933e
Guany <1084072690@qq.com>
2025-04-07 19:25:28 +0800

    删除了与conda初始化相关的代码块，简化了PowerShell配置文件。

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -16,13 +16,6 @@ function i {
     Set-Location -Path "~\i\$DirectoryName"
 }
 
-#region conda initialize
-# !! Contents within this block are managed by 'conda init' !!
-If (Test-Path "C:\Users\tly\miniconda3\Scripts\conda.exe") {
-    (& "C:\Users\tly\miniconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ?{$_} | Invoke-Expression
-}
-#endregion
-
 fnm completions --shell powershell | Out-String | Invoke-Expression
 
 fnm env --use-on-cd --version-file-strategy=recursive --corepack-enabled --resolve-engines --shell powershell | Out-String | Invoke-Expression
